d97317156993c5b0804c81d0f9fce189061953c9
SreekanthMolleti <92377436+SreekanthMolleti@users.noreply.github.com>
2021-10-19 11:11:49 +0530

    Add files via upload

---
+++ adding two numbers.c
@@ -0,0 +1,19 @@
+/*adding two numbers*/
+
+#include<stdio.h>
+void main()
+{
+	//variable declaration
+	int number1,number2,sum;
+	
+	//initialization
+	number1=29;
+	number2=56;
+	
+	//calculation
+	sum=number1+number2;
+	
+	//output
+	printf("sum of %d and %d is %d ",number1,number2,sum);
+	
+}
