6192f93020834027cb8071540a5d3fe8430064af
SreekanthMolleti <92377436+SreekanthMolleti@users.noreply.github.com>
2021-10-19 11:03:05 +0530

    Add files via upload

---
+++ data sequences.c
@@ -0,0 +1,19 @@
+//programme
+#include<stdio.h>
+int main()
+{
+    //variable declaration
+	int age;
+	float cgpa;
+	char initial;
+	
+	//initialization
+	age=20;
+	cgpa=89.62;
+	initial='m';
+	
+	//output 
+	printf("age of the student is %d\n",age);
+	printf("cgpa of the student is %f\n",cgpa);
+	printf("initial of the student is%\n",initial);
+}
